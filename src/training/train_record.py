c8736891e9e8e2471939bf7c89fb31b9a4a92283
藤井勇基 <yuki@fujiiyuukinoMacBook-Air.local>
2024-06-26 15:06:56 -0400

    Add doc string for instruction of train record class

--- src/training/train_record.py
+++ src/training/train_record.py
@@ -10,6 +10,17 @@ class TrainRecord:
         theoretical_mean_of_model: float,
         train_data: pd.DataFrame,
     ) -> None:
+        """
+        Initializes the TrainRecord with model parameters, theoretical mean of the model, and training data.
+
+        Args:
+            model_param (ParamStoreDict): The parameters of the trained model.
+            theoretical_mean_of_model (float): The theoretical mean of the model.
+            train_data (pd.DataFrame): The training data used for training.
+
+        Raises:
+            TypeError: If model_param is not an instance of ParamStoreDict.
+        """
         if not isinstance(model_param, ParamStoreDict):
             raise TypeError("model_param must be of type ParamStoreDict")
         
@@ -21,12 +32,30 @@ class TrainRecord:
 
     @property
     def model_param(self) -> ParamStoreDict:
+        """
+        Gets the model parameters.
+
+        Returns:
+            ParamStoreDict: The parameters of the trained model.
+        """
         return self._model_param
 
     @property
     def theoretical_mean_of_model(self) -> float:
+        """
+        Gets the theoretical mean of the model.
+
+        Returns:
+            float: The theoretical mean of the model.
+        """
         return self._theoretical_mean_of_model
 
     @property
     def train_data(self) -> pd.DataFrame:
+        """
+        Gets the training data.
+
+        Returns:
+            pd.DataFrame: The training data used for training.
+        """
         return self._train_data
