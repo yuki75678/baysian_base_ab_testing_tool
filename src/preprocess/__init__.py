79dbb58b03b4d40b8d38ca55d429ada52f727aaa
藤井勇基 <yuki@fujiiyuukinoMacBook-Air.local>
2024-06-25 21:28:33 -0400

    Fix: relative import to absolute import

--- src/preprocess/__init__.py
+++ src/preprocess/__init__.py
@@ -1,5 +1,5 @@
-from .preprocess_pipline import preprocess_pipline
-from .preprocessed import Preprocessed
+from src.preprocess.preprocess_pipline import preprocess_pipline
+from src.preprocess.preprocessed import Preprocessed
 
 
 __all__ = ["preprocess_pipline", "Preprocessed"]
