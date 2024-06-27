3031972bfe1c2378d3d9fd102680c51e67c41ceb
藤井勇基 <yuki@fujiiyuukinoMacBook-Air.local>
2024-06-27 10:54:53 -0400

    Mod exe main to run ab test pipeline

--- exe_main.py
+++ exe_main.py
@@ -1,16 +1,23 @@
 import pandas as pd
+from omegaconf import OmegaConf
 from src.arg_process import get_conf
-from src.train import train
-
+from src.ab_test_pipeline import ab_test_pipline
 
 def main():
     conf = get_conf()
     print(conf)
 
     data = pd.read_csv(conf.data)
-    print(data)
-
-    train()
+    ab_test_pipline(
+        data=data,
+        y_col=conf.y_col,
+        model=conf.model,
+        group_col=conf.group_col,
+        num_iterations=conf.train_pram.num_iterations,
+        lr=conf.train_pram.lr,
+        betas=tuple(OmegaConf.to_container(conf.train_pram.betas)),
+        )
+    
 
 
 if __name__ == "__main__":
