ba68a71397243ec1063799481f2ca3563275cd0e
藤井勇基 <yuki@fujiiyuukinoMacBook-Air.local>
2024-06-27 11:27:09 -0400

    Add error handling to get arg function in case file not found

--- src/arg_process.py
+++ src/arg_process.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from omegaconf import OmegaConf
 
 
@@ -7,6 +8,9 @@ def get_args():
 
     parser.add_argument("-c", "--conf", help="path of setting file")
     args = parser.parse_args()
+
+    if not os.path.exists(args.conf):
+        parser.error(f"The configuration file {args.conf} does not exist.")
 
     return args
 
